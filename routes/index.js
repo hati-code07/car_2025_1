486e5be2994da26eb8ecc5bfe2ca9505f6956f7f
maria123 <khatidzea@gmail.com>
2025-11-20 17:12:20 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* страница порш */
+router.get('/porsche', function(req, res, next) {
+  res.send("<h1>Страница Porsche</h1>") });
+
+/* страница гольф */
+router.get('/golf', function(req, res, next) {
+  res.send("<h1>Страница Golf GTI</h1>") });
+
+/* страница bmw */
+router.get('/bmw', function(req, res, next) {
+  res.send("<h1>Страница BMW M5 F90 Competition</h1>") });
+
+
+
 module.exports = router;
