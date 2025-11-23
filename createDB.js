963fc2d434f77b06ce46a1fce27d9d69f7033636
maria123 <khatidzea@gmail.com>
2025-11-24 00:07:42 +0300

    car_6_2

--- createDB.js
+++ createDB.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-
+var data = require("./data.js").data;
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 const dbName = 'test2024';
