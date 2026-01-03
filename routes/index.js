79e38e76a4adc077141a9d22205697f3bd1c5cfc
maria123 <khatidzea@gmail.com>
2026-01-04 01:12:29 +0300

    car_10_6

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
- res.render('logreg',{title: 'Вход'});
+ res.render('logreg',{title: 'Вход', error: null});
  });
 
 /* POST login/registration page. */
@@ -35,7 +35,7 @@ router.post('/logreg', async function(req, res, next) {
        req.session.user_id = foundUser._id
        res.redirect('/')
             } else {
-       res.render('logreg',{title: 'Вход'});
+       res.render('logreg',{title: 'Вход',error: 'Пароль не верный'});
      }
   }  
 });
