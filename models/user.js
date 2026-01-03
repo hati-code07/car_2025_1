248a709091b058337e02775552146b075dca6344
maria123 <khatidzea@gmail.com>
2026-01-04 00:52:41 +0300

    car_10_4

--- models/user.js
+++ models/user.js
@@ -34,6 +34,9 @@ userSchema.virtual("password").set(function(password){
 userSchema.methods.encryptPassword = function(password){
     return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
 }
+userSchema.methods.checkPassword = function(password){
+   return this.encryptPassword(password) === this.hashedPassword
+}
 
 
 
